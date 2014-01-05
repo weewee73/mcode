61d2578c1f19801fe4111dfe9df1453238cdd904
flyfire <zero.flyfire@gmail.com>
2014-01-05 11:44:06 +0800

    add,modify

--- shell/mman.sh
+++ shell/mman.sh
@@ -14,7 +14,7 @@ then
     then
         exit $ret
     fi
-    man $name | col -bx | sed "s/\xe2\x80\x98/'/g" | sed "s/\xe2\x80\x99/'/g" >$f_man
+    man $name | col -bx | sed "s/\xe2\x80\x98/'/g" | sed "s/\xe2\x80\x99/'/g" | sed 's/\(\w\+\)\s\{2,3\}\(\w\+\)\s*/\1 \2 /g' >$f_man
 fi
 
 vim $f_man

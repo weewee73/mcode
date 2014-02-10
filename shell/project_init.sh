2b81b4a408fa2183ed7b5f973a7ec37cc07da204
flyfire <zero.flyfire@gmail.com>
2014-02-10 02:29:25 -0800

    modify

--- shell/project_init.sh
+++ shell/project_init.sh
@@ -1,5 +1,5 @@
-ctags -R --c-kinds=+px
-cscope -Rbq
+#ctags -R --c-kinds=+px
+#cscope -Rbq
 
 #cflow file in top dir
 #find . -name "*.c" | 
@@ -9,5 +9,21 @@ cscope -Rbq
 #        cflow $cfile -o ${cflowfile}.cflow; 
 #    done
 
-find . -name "*.c" | tee c.file | xargs -I{} cflow {} -o {}.cflow
-cat c.file | xargs cflow  -o all.cflow
+#find . -name "*.c" | tee c.file | xargs -I{} cflow {} -o {}.cflow
+#cat c.file | xargs cflow  -o all.cflow
+
+
+source=`pwd`
+
+echo -ne "\033]0;$(basename $source) coding...\007"
+
+ctags -R --c-kinds=+px
+cscope -Rbq
+
+f_session="Session.vim"
+if [[ -f $f_session ]]; then
+    vim -c "cs add $source/cscope.out $source" -S $f_session
+else
+    vim -c "cs add $source/cscope.out $source" .
+fi
+

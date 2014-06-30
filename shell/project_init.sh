2d40e3c6fc69bf86d162bcd2cbd6dc0857f585ff
flyfire <zero.flyfire@gmail.com>
2014-07-01 02:51:32 +0800

    fix slowly vim syntax

--- shell/project_init.sh
+++ shell/project_init.sh
@@ -1,31 +1,45 @@
-#ctags -R --c-kinds=+px
-#cscope -Rbq
+#!/bin/bash
 
-#cflow file in top dir
-#find . -name "*.c" | 
-#    while read cfile
-#    do 
-#        cflowfile=`echo $cfile | sed 's#/#_#g'`; 
-#        cflow $cfile -o ${cflowfile}.cflow; 
-#    done
+find .  -name "*.[ch]" >cscope.files
 
-#find . -name "*.c" | tee c.file | xargs -I{} cflow {} -o {}.cflow
-#cat c.file | xargs cflow  -o all.cflow
+cscope -bq
+ctags --sort=yes --c-kinds=+px -L cscope.files
 
+awk -v fcolor="c.vim" '
+BEGIN{
+    Tenum="syn keyword cEnum "
+    Ttypedef="syn keyword cType "
+    Tdefine="syn keyword cDefine "
+    Tglobal="syn keyword cGlobalVal "
+}
+{
+    if (/;"\te/)
+        Tenum = Tenum " " $1
+    else if (/;"\tt/)
+        Ttypedef  = Ttypedef " " $1
+    else if (/;"\td/)
+        Tdefine  = Tdefine " " $1
+    else if (/;"\tv/)
+        Tglobal  = Tglobal " " $1
+}
+END{
+    if (length(Tenum) > length("syn keyword cEnum "))
+        print Tenum >fcolor
+        print "hi cEnum ctermfg=DarkRed" >> fcolor
+    if (length(Ttypedef) > length("syn keyword cType "))
+        print Ttypedef >>fcolor
+    if (length(Tenum) > length("syn keyword cDefine "))
+        print Tdefine >>fcolor
+    if (length(Tenum) > length("syn keyword cGlobalVal "))
+        print Tglobal >>fcolor
+        print "hi def link cGlobalVal cDefine" >> fcolor
+}
+' tags
 
-source=`pwd`
-
-echo -ne "\033]0;$(basename $source) coding...\007"
-
-ctags -R --c-kinds=+px
-cscope -Rbq
-
-f_session="Session.vim"
-if [[ -f $f_session ]]; then
-#    vim -c "cs add $source/cscope.out $source" -S $f_session
-    vim -S $f_session
+[ -e ~/.vim/syntax ] || mkdir -p ~/.vim/syntax
+if [ -f ~/.vim/syntax/common.vim ]; then
+    cat c.vim "$HOME/.vim/syntax/common.vim" >"$HOME/.vim/syntax/c.vim"
 else
-#    vim -c "cs add $source/cscope.out $source" .
-    vim $source
+    cp c.vim "$HOME/.vim/syntax/c.vim"
 fi
 
